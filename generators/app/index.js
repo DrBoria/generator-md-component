19929a517f4ed514d3fec034cda6a68320c5a151
Mikita Dusmikeev <dr.boria@gmail.com>
2024-10-07 20:19:47 +0200

    test functionality of import

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,74 +1,117 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const path = require('path');
-const fs = require('fs');
-const ncp = require('ncp').ncp;
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const path = require("path");
+const fs = require("fs");
+const ncp = require("ncp").ncp;
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        `Welcome to the glorious ${chalk.red('generator-md-component')} generator!`
+        `Welcome to the glorious ${chalk.red(
+          "generator-md-component"
+        )} generator!`
       )
     );
 
-    const componentsPath = path.resolve(
+    // Read dependencies from ../../node_modules/md-turborepo/apps/docs/dependencies.json
+    const dependenciesPath = path.resolve(
       __dirname,
-      '../../node_modules/md-components-library/src/components'
+      "../../node_modules/md-turborepo/apps/docs/dependencies.json"
     );
-    // Read the directory contents
-    const moduleContent = fs.readdirSync(componentsPath);
+    const dependenciesFile = JSON.parse(
+      fs.readFileSync(dependenciesPath, "utf8")
+    );
+    this.componentsToCopy = dependenciesFile.components;
 
-    const prompts = [
-      {
-        type: "checkbox",
-        name: "selectedComponents",
-        message: "select rules for your project:",
-        choices: moduleContent.map(component => (
-          {
-            name: component,
-            value: component,
-            checked: false,
-          }
-        )),
-      }
-    ];
+    this.log("Found components:", this.componentsToCopy);
 
+    // No need for user prompts anymore, since we're taking components from dependencies.json
+  }
 
-    return this.prompt(prompts).then(props => {
-      this.componentsToCopy = props.selectedComponents; // [ 'Button', 'Charts', 'Form', 'Pagination' ]
-    });
+  _collectDependenciesFromJson(componentsPath, componentName) {
+    // Check if the component has a dependencies.json file and extract dependencies
+    const componentDependenciesPath = path.join(
+      componentsPath,
+      componentName,
+      "dependencies.json"
+    );
+    let componentDependencies = [];
+
+    if (fs.existsSync(componentDependenciesPath)) {
+      const componentDependenciesFile = JSON.parse(
+        fs.readFileSync(componentDependenciesPath, "utf8")
+      );
+      componentDependencies = componentDependenciesFile.packages || [];
+    }
+
+    return componentDependencies;
   }
 
   writing() {
-    ncp.limit = 16; // controls how many concurrent file streams
+    const componentsPath = path.resolve(
+      __dirname,
+      "../../node_modules/md-components-library/src/components"
+    );
+    const destinationRoot = path.join(
+      this.destinationRoot(),
+      "packages/components"
+    );
+
+    ncp.limit = 16; // Controls how many concurrent file streams
+
+    let allDependencies = new Set(); // To store unique package dependencies
+
     this.componentsToCopy.forEach(componentName => {
-      const sourceModulePath = path.join( __dirname, '../../node_modules/md-components-library/src/components', componentName);
-      const destinationPath = path.join('components', componentName);
+      const sourceModulePath = path.join(componentsPath, componentName);
+      const destinationPath = path.join(destinationRoot, componentName);
 
       // Ensure the destination directory exists
-      fs.mkdirSync(destinationPath, { recursive: true }, (err) => {
+      fs.mkdirSync(destinationPath, { recursive: true }, err => {
         if (err) {
-          return console.error('Error creating directory:', err);
+          return console.error("Error creating directory:", err);
         }
       });
-      
-      ncp(sourceModulePath, destinationPath, function (err) {
+
+      // Copy the component files
+      ncp(sourceModulePath, destinationPath, function(err) {
         if (err) {
           return console.error(err);
         }
-        console.log(componentName, ' copied successfully');
+
+        console.log(componentName, " copied successfully");
       });
+
+      // Collect dependencies from component dependencies.json
+      const componentDependencies = this._collectDependenciesFromJson(
+        componentsPath,
+        componentName
+      );
+      componentDependencies.forEach(dep => allDependencies.add(dep));
     });
 
+    // Create a package.json with the collected dependencies
+    const packageJson = {
+      name: "md-components",
+      version: "1.0.0",
+      dependencies: {}
+    };
 
-    // this.fs.copy(
-    //   this.templatePath('dummyfile.txt'),
-    //   this.destinationPath('dummyfile.txt')
-    // );
+    allDependencies.forEach(dep => {
+      packageJson.dependencies[dep] = "latest"; // You may customize the versioning as needed
+    });
+
+    fs.writeFileSync(
+      path.join(destinationRoot, "package.json"),
+      JSON.stringify(packageJson, null, 2)
+    );
+    console.log(
+      "package.json created with dependencies:",
+      Array.from(allDependencies)
+    );
   }
 
   install() {
